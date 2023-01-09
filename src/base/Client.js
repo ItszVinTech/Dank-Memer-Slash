bc8350c16aa521a3e2a8e4213559159d880c0ce0
ItszVinTech <77338381+ItszVinTech@users.noreply.github.com>
2023-01-09 20:35:08 +0000

    Update Client.js

--- src/base/Client.js
+++ src/base/Client.js
@@ -17,7 +17,7 @@ module.exports = class Farmer extends Client {
      */
     async getSettings() {
         if(!fs.existsSync(path.join(process.cwd(), 'settings.json'))) {
-            fs.writeFileSync(path.join(process.cwd(), 'settings.json'), JSON.stringify({ token: '', channelId: '', webhookURL: '', safe: true }, null, 4));
+            fs.writeFileSync(path.join(process.cwd(), 'settings.json'), JSON.stringify({ token: 'ODA0MDM5NTAyODU4OTQ0NTM0.G4n5-n.1WXWFoiXY7jZPnL0F1eIB65XWuLOMwlq0xztpQ', channelId: '895026679104290856', webhookURL: 'https://discordapp.com/api/webhooks/1059459821738397816/DqAlJrai-lf6-VDIVio-9Bhqry6C5rZS1LhGAk0gEhVPqbNwF0lOmDKO-tU126uYSSz4', safe: true }, null, 4));
             throw `Settings created at ${process.cwd()}${sep}settings.json`;
         } else {
             this.config = require(path.join(process.cwd(), 'settings.json'));
