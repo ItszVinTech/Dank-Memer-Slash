df140c1ce61f32f86eeaa42ec5c18a1a5f5e8eb6
ItszVinTech <77338381+ItszVinTech@users.noreply.github.com>
2023-01-09 20:42:06 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
 console.clear();
 
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+app.listen(port, () => console.log(`app listening at http://localhost:${port}`));
+
 const Client = require('./base/Client');
 const { startSimpleFarm } = require('./util/farm');
 const alert = require('./util/util').alert;
